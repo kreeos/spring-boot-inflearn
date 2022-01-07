1de5ee0ef78bd68e440babeccce79a12b89bd27a
kreeos <lottie47@kaist.ac.kr>
2022-01-07 16:03:52 +0900

    김영한님 첫 강의 끝
    Create Project
    Spring web MVC / API
    BE - User management
    Spring Bean & Dependency
    User Management & Web MVC connection
    Spring DB - H2, Jdbc, JPA
    AOP

---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,28 @@
+package hello.hellospring.service;
+
+import hello.hellospring.TimeTraceAop;
+import hello.hellospring.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private  final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+}
